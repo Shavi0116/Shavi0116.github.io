52931e5c3fcdec6491e14723416136943b577fcd
Shavi Sharma <mail4shavi@gmail.com>
2025-06-18 10:13:07 +0530

    html tags

--- p1/script.js
+++ p1/script.js
@@ -2,8 +2,8 @@ let users = [];
 let user = {};
 
 const validateUser = () => {
-  let email = document.getElementById("txtEmail1").value;
-  let pass = document.getElementById("txtPass1").value;
+  let email = document.getElementById("txtEmail").value;
+  let pass = document.getElementById("txtPass").value;
   const found = users.find(
     (value) => value.email === email && value.pass === pass
   );
@@ -14,32 +14,37 @@ const validateUser = () => {
   }
 };
 
-const loginForm = () => {
-  let userIs = "<ul>";
-  users.forEach((u) => {
-    userIs += `<li>${u.name} (${u.email})</li>`;
-  });
-  userIs += "</ul>";
+const renderUserList = () => {
+    let x= "<h4>Registered Users:</h4>";
+    if(users.length==0){
+        return "<p>No Users Registered</p>";
 
-  const str = `
-    <div style='display:flex'>
-      <div>
-        <h3>Login Form</h3>
-        <p id='errorTxt' style='color:red'></p>
-        <p><input type='text' id='txtEmail1' placeholder='Email'></p>
-        <p><input type='password' id='txtPass1' placeholder='Password'></p>
-        <p><button onclick='validateUser()'>Submit</button></p>
-        <p><button onclick='registerForm()'>Create Account</button></p>
-      </div>
-      <div>
-        <h3>List of Users</h3>
-        ${userIs}
-      </div>
-    </div>
-  `;
-  root.innerHTML = str;
+    }
+    users.forEach(user => {
+        x += `<p>${user.name} | ${user.email} | ${user.pass} | ${user.balance}</p>`;
+    });
+    
+    return x;
 };
 
+const loginForm = () => {
+  const str = `
+    <div style='display:flex'>
+    <div>
+    <h3>Login Form</h3>
+    <p id='errorTxt'></p>
+    <p><input type='text' id='txtEmail'></p>
+    <p><input type='password' id='txtPass'></p>
+    <p><button onclick='validateUser()'>Submit</button></p>
+    <p><button onclick='registerForm()'>Create Account</button></p>
+    </div>
+    <div>
+     ${renderUserList()}
+    </div>
+    </div>
+    `;
+  root.innerHTML = str;
+};
 
 const saveUser = () => {
   let name = document.getElementById("txtName").value;
@@ -49,6 +54,7 @@ const saveUser = () => {
     name,
     email,
     pass,
+    balance:1000,
   });
   loginForm();
 };
@@ -56,9 +62,9 @@ const saveUser = () => {
 const registerForm = () => {
   const str = `<div>
     <h3>Registration Form</h3>
-    <p><input type='text' id='txtName' placeholder="Name"></p>
-    <p><input type='text' id='txtEmail' placeholder="Email"></p>
-    <p><input type='password' id='txtPass' placeholder="Password"></p>
+    <p><input type='text' id='txtName'></p>
+    <p><input type='text' id='txtEmail'></p>
+    <p><input type='password' id='txtPass'></p>
     <p><button onclick='saveUser()'>Submit</button></p>
     <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
     `;
@@ -72,3 +78,6 @@ const showHome = () => {
     `;
   root.innerHTML = str + "</div>";
 };
+
+
+loginForm()
